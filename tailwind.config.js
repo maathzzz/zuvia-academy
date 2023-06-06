b069b59b9afac7055e608d269e4014d712aad697
Matheus <matheusdeamorim20@gmail.com>
2023-06-06 11:37:14 -0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      'sm': {'max': '414px'},
+
+      'md': {'max': '768px'},
+
+      'lg': {'max': '1024px'},
+
+      'xl': {'max': '1280px'},
+
+      '2xl': {'max': '1536px'},
     },
+    extend: {},
   },
   plugins: [],
 }

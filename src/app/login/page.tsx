e595aad72b08c644e4bf0660b828b847b25b7ded
Matheus <matheusdeamorim20@gmail.com>
2023-06-12 14:43:18 -0300

    feat: href

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -37,7 +37,7 @@ export default function Login() {
 
     }
   })
-  
+
   useEffect(() => {
     if(isAuthenticated === true){
       router.push('/dashboard')
@@ -62,7 +62,7 @@ export default function Login() {
       <header className="absolute inset-x-0 top-0 z-50 ">
           <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
-              <a href="#" className="-m-1.5 p-1.5">
+              <a href="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <Image
                   className="w-auto"
